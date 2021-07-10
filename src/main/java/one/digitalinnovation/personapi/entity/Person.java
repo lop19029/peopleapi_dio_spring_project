0af759207242c101b6424560e0a20017cac1f360
lop19029 <alexhumbertolds@gmail.com>
2021-07-09 21:34:29 -0300

    Create Person and Phone classes

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String cpf;
+
+    //private LocalDate birthDate;
+
+    private List<Phone> phone;
+}
