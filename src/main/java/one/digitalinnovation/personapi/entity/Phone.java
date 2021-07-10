d7ca9e8192107b409461eed239a1b9f88201ac12
lop19029 <alexhumbertolds@gmail.com>
2021-07-09 21:50:30 -0300

    Map Phone class

--- src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -1,19 +1,26 @@
 package one.digitalinnovation.personapi.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import one.digitalinnovation.personapi.enums.PhoneType;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
+
+    @Id
+    //Automatically assigns an id value when uploading to database
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private  Long id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PhoneType type;
 
+    @Column(nullable = false)
     private String number;
 }
