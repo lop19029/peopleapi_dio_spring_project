8db2334b549a1ad2fbf832d17b1428d4a3f4775a
lop19029 <alexhumbertolds@gmail.com>
2021-07-12 11:39:47 -0300

    Delegate service operations to PersonService

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,7 +1,23 @@
 package one.digitalinnovation.personapi.service;
 
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public String createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return "Created person with ID "+savedPerson.getId();
+    }
 }
